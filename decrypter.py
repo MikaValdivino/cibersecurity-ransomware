ef1332670176df7259b6201e67b3a06cf67e6965
Mikaeli Valdivino <150645002+MikaValdivino@users.noreply.github.com>
2024-07-27 21:30:04 -0300

    decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -8,11 +8,13 @@ file_data = file.read()
 file.close()
 
 ## chave para descriptografia
+
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
 ## remover o arquivo criptografado
+
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
